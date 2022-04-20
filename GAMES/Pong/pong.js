9f305c6f037b08f4b6354c0e75bc216a3d01a164
liljfff <pexped@gmail.com>
2022-04-20 16:05:36 -0700

    1.1.6

--- GAMES/Pong/pong.js
+++ GAMES/Pong/pong.js
@@ -1,6 +1,7 @@
 // screen width is 256, height is 192
 
-let imgBall = spriteArt(`
+let imgBall = spriteArt(
+	`
 ..wwww..
 .ww..ww.
 ww....ww
@@ -8,23 +9,63 @@ w......w
 w......w
 ww....ww
 .ww..ww.
-..wwww..`);
+..wwww..`
+);
 
 // the \n means new line
 let imgPaddle = spriteArt('.wwwwww.\nwwwwwwww\n' + 'ww....ww\n'.repeat(42) + 'wwwwwwww\n.wwwwww.');
 
-/* PART A1: Make image for the wall */
+/* PART B: Make image for the wall */
+let imgWallX = spriteArt('w'.repeat(256));
 
 // places a ball in center of the screen
 let ball = createSprite(imgBall);
 ball.x = width / 2;
 ball.y = height / 2;
+ball.velocity.x = 1;
+ball.velocity.y = 1;
 
 /* PART A0: create two paddles, place on each end of the screen */
+let paddleL = createSprite(imgPaddle);
+paddleL.x = 16;
+paddleL.y = 80;
+
+let paddleR = createSprite(imgPaddle);
+paddleR.x = 240;
+paddleR.y = 80;
+
+let topwall = createSprite(imgWallX);
+topwall.x = 0;
+topwall.y = 10;
+
+let bottomwall = createSprite(imgWallX);
+bottomwall.x = 0;
+bottomwall.y = 182;
 
 function draw() {
-	/* PART A1: draw the ball and paddles inside the p5 main draw function */
-	// the `width` and `height` variables are the width and height of the screen
+	background(0);
+
+	paddleL.y = mouseY;
+	paddleR.y = mouseY;
+
+	if (ball.y == topwall.y) {
+		ball.velocity.y = 1;
+	}
+	if (ball.y + ball.h == bottomwall.y) {
+		ball.velocity.y = -1;
+	}
+
+	if (ball.x <= paddleL.x + paddleL.w && ball.x > paddleL.x && ball.y > paddleL.y && ball.y < paddleL.y + paddleL.h) {
+		ball.velocity.x = 1;
+	}
+	if (
+		ball.x + ball.w >= paddleR.x &&
+		ball.x + ball.w < paddleR.x + paddleR.w &&
+		ball.y > paddleR.y &&
+		ball.y < paddleR.y + paddleR.h
+	) {
+		ball.velocity.x = -1;
+	}
 
 	drawSprites();
 }
